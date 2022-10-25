537063c994f68343ee742a268c3a29d75d396711
Cian O'Gorman <29048772+CianOG123@users.noreply.github.com>
2022-10-25 16:25:37 +0100

    Fixed tests

--- calculator_tests.py
+++ calculator_tests.py
@@ -2,4 +2,4 @@ from calculator import multiply
 
 # Test used for setting up repository (this will be deleted later).
 def test_multiply():
-    assert multiply(10, 11) == 115
+    assert multiply(10, 11) == 110
