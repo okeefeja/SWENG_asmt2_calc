89e0741a950489b9b2da5aab16dd044b0996d910
Cian O'Gorman <29048772+CianOG123@users.noreply.github.com>
2022-10-25 16:06:40 +0100

    test functions added

---
+++ calculator.py
@@ -0,0 +1,4 @@
+
+# Test function used for setting up repository (this will be deleted later).
+def multiply(number1, number2):
+    return number1 * number2
---
+++ calculator_tests.py
@@ -0,0 +1,5 @@
+from calculator import multiply
+
+# Test used for setting up repository (this will be deleted later).
+def test_multiply():
+    assert multiply(10, 11) == 110
